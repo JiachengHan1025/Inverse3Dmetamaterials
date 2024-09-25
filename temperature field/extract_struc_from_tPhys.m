fac57517955a4f072eaf00977ee738590d0121ae
JiachengHan1025 <hjc1025@mail.ustc.edu.cn>
2024-09-25 15:44:00 +0800

    init

--- temperature field/extract_struc_from_tPhys.m
+++ temperature field/extract_struc_from_tPhys.m
@@ -1,8 +1,8 @@
 tPhys=csvread("F:\hanjiacheng\学习资料\热传导微结构项目进展\M&Drevision\Github\temperature field\IWP\bulk\tphys.csv");
 tPhys=reshape(tPhys,128,128,128);
 %projection
-beta=64;
+beta=256;
 eta=0.5;%different eta, different isosurface
 
 xPhys = 1-(tanh(beta*eta)+tanh(beta*(tPhys-eta)))/(tanh(beta*eta)+tanh(beta*(1-eta)));
-volumeViewer(tPhys);
+volumeViewer(xPhys);
